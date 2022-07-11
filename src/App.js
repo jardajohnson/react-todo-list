758856e04fe92c0ec3f4089134a52311b99749c7
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-11 15:35:45 -0700

    add state and default list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import List from './components/List';
+import React, { useState } from 'react';
+
 function App() {
+	const [list, setList] = useState([
+		{ text: 'Hello', checked: false },
+		{ text: 'World', checked: false },
+		{ text: 'HelloWorld', checked: false },
+	]);
+
 	return (
 		<div>
 			<header>
+				<List list={list} />
 				<h1 className='text-danger'>React</h1>
 			</header>
 		</div>
