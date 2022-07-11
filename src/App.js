705c5f593802e0981e5cf2b58f10e720b09ed849
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-11 15:24:23 -0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ function App() {
 	return (
 		<div>
 			<header>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
+				<h1 className='text-danger'>React</h1>
 			</header>
 		</div>
 	);
