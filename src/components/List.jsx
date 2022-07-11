0c9609bb86349050b33e2e263802e5434a87010e
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-11 15:54:31 -0700

    add list map to render each list item

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
-const List = (props) => {
-	return <>{JSON.stringify(props)}</>;
+const List = ({ list }) => {
+	return (
+		<ul>
+			{list.map((item, index) => {
+				return (
+					<div>
+						<h3 key={index} checked={item.checked}>
+							# {index + 1} {item.text}
+						</h3>
+						<button className='btn btn-primary'>Delete</button>
+					</div>
+				);
+			})}
+		</ul>
+	);
 };
 
 export default List;
